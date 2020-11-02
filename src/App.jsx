e4aebcb87eb62f08cbe17e7f8ab2b7d72626baa9
Amine Elmouradi <elmouradi.amine98@gmail.com>
2020-11-02 22:18:58 +0100

    create footer layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Footer from './layout/Footer/Footer';
 import Header from './layout/Header/Header';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/layout/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Footer.css';
+
+const Footer = ({ className }) => (
+  <footer className={`footer ${className}`}>
+    {/* copyright */}
+    <div className="footer__copyright">
+      © 2020 Chat Party, Inc.
+    </div>
+  </footer>
+);
+
+Footer.propTypes = {
+  className: PropTypes.string,
+};
+
+Footer.defaultProps = {
+  className: '',
+};
+
+export default Footer;
