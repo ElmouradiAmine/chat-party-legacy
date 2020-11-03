74ad37edfbafcbe550251fba5c6289b981f57ad7
Amine Elmouradi <elmouradi.amine98@gmail.com>
2020-11-03 20:51:43 +0100

    update connection form testing

--- src/layout/ConnectionForm/__test__/ConnectionForm.test.jsx
+++ src/layout/ConnectionForm/__test__/ConnectionForm.test.jsx
@@ -2,32 +2,54 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import userEvent from '@testing-library/user-event';
-
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import ConnectionForm from '../ConnectionForm';
 
 afterEach(cleanup);
 describe('ConnectionForm component', () => {
   const className = 'className';
+  const mockStore = configureStore();
+  const initialState = {
+    user: { user: null, error: '', status: 'idle' },
+  };
+  const store = mockStore(initialState);
 
   test('renders correctly', () => {
-    render(<ConnectionForm />);
+    render(
+      <Provider store={store}>
+        <ConnectionForm />
+      </Provider>,
+    );
   });
 
   test('renders correctly with props', () => {
-    const { container } = render(<ConnectionForm className={className} />);
+    const { container } = render(
+      <Provider store={store}>
+        <ConnectionForm className={className} />
+      </Provider>,
+    );
     expect(container).not.toBeNull();
     expect(container.firstElementChild?.classList).toContain(className);
   });
 
   test('should control the input component value', () => {
-    const { getByPlaceholderText } = render(<ConnectionForm className={className} />);
+    const { getByPlaceholderText } = render(
+      <Provider store={store}>
+        <ConnectionForm className={className} />
+      </Provider>,
+    );
     const input = getByPlaceholderText('Username');
     userEvent.type(input, 'test');
     expect(input).toHaveValue('test');
   });
 
   test('should control the gender cards component value', () => {
-    const { getByTestId } = render(<ConnectionForm className={className} />);
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <ConnectionForm className={className} />
+      </Provider>,
+    );
     const genderCardMale = getByTestId('card-male');
     const genderCardFemale = getByTestId('card-female');
 
@@ -44,7 +66,11 @@ describe('ConnectionForm component', () => {
   });
 
   test('matches snapshot', () => {
-    const tree = renderer.create(<ConnectionForm className={className} />);
+    const tree = renderer.create(
+      <Provider store={store}>
+        <ConnectionForm className={className} />
+      </Provider>,
+    );
     expect(tree).toMatchSnapshot();
   });
 });
