77547c47c278c9e5489eef2f53e5a97934dda92b
Amine Elmouradi <elmouradi.amine98@gmail.com>
2020-11-03 12:56:35 +0100

    add socket redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import createSocketIoMiddleware from 'redux-socket.io';
+import io from 'socket.io-client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const socket = io('http://localhost:5000');
+const socketIoMiddleware = createSocketIoMiddleware(socket, 'server/');
+
+const store = configureStore({
+  reducer: {
+
+  },
+  middleware: [socketIoMiddleware],
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
 
+store.dispatch({ type: 'server/hello', data: 'Hello!' });
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
