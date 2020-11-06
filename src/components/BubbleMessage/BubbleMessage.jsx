b375fe297b3ccc75566ee8898b991000577ba29b
Amine Elmouradi <elmouradi.amine98@gmail.com>
2020-11-06 19:21:55 +0100

    add bubble message

---
+++ src/components/BubbleMessage/BubbleMessage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+import './BubbleMessage.css';
+
+const BubbleMessage = ({ isMe, message, createdAt }) => {
+  const time = moment(createdAt).format('LT');
+  return (
+    <div className="msg">
+      <div className={`bubble ${isMe && 'altfollow'}`}>
+        <div className="txt">
+          <span className="timestamp">{time}</span>
+          <p className={`message ${isMe && 'follow'}`}>{message}</p>
+        </div>
+        <div className={`bubble-arrow ${isMe && 'alt'}`} />
+      </div>
+    </div>
+  );
+};
+
+BubbleMessage.propTypes = {
+  isMe: PropTypes.bool,
+  message: PropTypes.string,
+  createdAt: PropTypes.number,
+};
+
+BubbleMessage.defaultProps = {
+  isMe: false,
+  message: '',
+  createdAt: 0,
+};
+
+export default BubbleMessage;
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 
 import './Chat.css';
 
-const Chat = ({ className }) => <div className={`chat ${className}`}>Chat</div>;
+const Chat = ({ className }) => (
+  <div className={`chat ${className}`}>
+    <div className="chat__video-container chat__video-container--1">Video Container</div>
+    <div className="chat__video-container chat__video-container--2">Video Container</div>
+    <div className="chat__messages">message Container</div>
+  </div>
+);
 
 Chat.propTypes = {
   className: PropTypes.string,
