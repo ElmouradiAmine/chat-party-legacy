2b953427a124111f4ae65f589e82edf71f06f471
Amine Elmouradi <elmouradi.amine98@gmail.com>
2020-11-15 18:03:53 +0100

    revert back the get country

--- src/utils/getCountry.js
+++ src/utils/getCountry.js
@@ -1,17 +1,17 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// const getCountry = async () => {
-//   try {
-//     const url = 'https://ipinfo.io/json?token=8909f4357304ee';
-//     const response = await axios.get(url);
-//     return response.data.country.toLowerCase();
-//   } catch (error) {
-//     return 'ma';
-//   }
-// };
+const getCountry = async () => {
+  try {
+    const url = 'https://ipinfo.io/json?token=8909f4357304ee';
+    const response = await axios.get(url);
+    return response.data.country.toLowerCase();
+  } catch (error) {
+    return 'ma';
+  }
+};
 
-const getCountry = async () => new Promise((resolve) => {
-  resolve('ma');
-});
+// const getCountry = async () => new Promise((resolve) => {
+//   resolve('ma');
+// });
 
 export default getCountry;
